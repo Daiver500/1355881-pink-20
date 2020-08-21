57fad207021a24364d7a4cc8ad98c00017577080
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-08-21 13:55:56 +0300

    Подгоняю под пиксель перфект

--- source/sass/blocks/modal-accept.js
+++
@@ -1,7 +0,0 @@
-var openModalaccept = document.querySelector(".form__button");
-var modalAcceptshow = document.querySelector(".modal__decline");
-
-openModalAccept.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modalAcceptshow.classList.add("modal-decline--show");
-});
