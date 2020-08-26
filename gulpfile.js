47c75fceb141ce1e816b8e9d1512267d2edbaf2a
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-08-26 03:26:20 +0300

    Не работает

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,10 @@ const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
 const csso = require("gulp-csso");
 const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
 
 // Styles
 
@@ -21,18 +25,91 @@ const styles = () => {
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+//Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+   .pipe(imagemin([
+     imagemin.optipng({optimizationLevel: 3}),
+     imagemin.mozjpeg({progressive: true}),
+     imagemin.svgo(),
+   ]))
+}
+
+exports.images = images;
+
+//Webp
+
+const imageswebp = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.imageswebp = imageswebp;
+
+//SVG sprite
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.sprite = sprite;
+
+//Copy
+
+const copy = () => {
+  return gulp.src ([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy
+
+//Clean
+
+const clean = () => {
+  return del("build");
+};
+
+exports.clean = clean
+
+//Build
+
+const build = () => gulp.series (
+  "clean",
+  "copy",
+  "styles",
+  "images",
+  "sprite",
+  "imageswebp"
+);
+
+exports.build = build;
+
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
