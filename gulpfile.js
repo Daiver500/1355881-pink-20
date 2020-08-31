5b496374db9fc604c70ea1318488c0469267be22
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-08-31 17:05:46 +0300

    1

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,7 @@ const styles = () => {
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
     .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
